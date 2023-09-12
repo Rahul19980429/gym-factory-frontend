9d89563b34d1a944d35cf884038553a590c17b62
Rahul19980429 <128597358+Rahul19980429@users.noreply.github.com>
2023-09-12 17:49:13 +0530

    Update VerifyUser.js

--- src/pages/VerifyUser.js
+++ src/pages/VerifyUser.js
@@ -71,21 +71,16 @@ const VerifyUser = () => {
                             <div className='card-body text-center px-3 pt-4'>
                                 <form onSubmit={submit}>
                                     <h1 className='text-center  mb-5 text-danger'> Verification </h1>
-
-
-                                    <div className="input-group mb-3 border border-white rounded-1">
+                                    <div className="input-group mb-5 border border-white rounded-1">
                                         <input type='password' autoComplete='off' className="setbgImg form-control border-0 text-white " onChange={onChange} placeholder="Enter Password" name="password" value={password} />
                                     </div>
                                     <div className="input-group mb-3 text-danger justify-content-center ">
                                         {error.error ? error.error : '' || error.errors ? error.errors[0].msg : ''}
                                     </div>
-
-
                                     <div className="d-flex gap-2 mt-2 justify-content-center">
                                         <button className='btn text-white border-0 mb-4 btnlogIn me-3 px-5' disabled={!password ? true : false} type="submit">Verify</button>
                                         <button className='btn text-white border-0 mb-4 btnlogIn px-5' disabled={!password ? true : false} type="button" onClick={() => setPassword('')}>Clear</button>
                                     </div>
-
                                 </form>
                             </div>
                         </div>
